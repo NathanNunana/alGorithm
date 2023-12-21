ecc05fc646c573f48ffe806d235d284265cc2ad3
Nathan Kulewoshie <kulewoshienathan@gmail.com>
2023-12-21 08:00:47 +0000

    linked list prepend, append, delete by value

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -1,5 +1,9 @@
 package linkedlist
 
+import (
+	"fmt"
+)
+
 // node struct
 type node struct {
 	data interface{}
@@ -15,10 +19,61 @@ type LinkedList struct {
 // returns the lenth of the linked list
 func (l LinkedList) GetLength() int { return l.len }
 
-func (l *LinkedList) Insert(value interface{}) {
-	newNode := node{data: value}
+// adds a node at the beginning of the list
+func (l *LinkedList) Prepend(value interface{}) {
+	newNode := &node{data: value}
 	if l.head == nil {
-		l.head = &newNode
+		l.head = newNode
+	} else {
+		second := l.head
+		l.head = newNode
+		l.head.next = second
+	}
+	l.len++
+}
+
+// adds the node at the end of the list
+func (l *LinkedList) Append(value interface{}) {
+	newNode := &node{data: value}
+	if l.head == nil {
+		l.head = newNode
+	} else {
+		current := l.head
+		for current.next != nil {
+			current = current.next
+		}
+		current.next = newNode
+	}
+	l.len++
+}
+
+// removes a node by value
+func (l *LinkedList) RemoveNode(value interface{}) {
+	if l.head == nil {
+		return
 	}
 
+	if l.head.data == value {
+		l.head = l.head.next
+		l.len--
+		return
+	}
+
+	current := l.head
+	for current.next != nil && current.next.data != value {
+		current = current.next
+	}
+
+	if current.next != nil {
+		current.next = current.next.next
+	}
+}
+
+// prints the entire linked list
+func (l LinkedList) PrintList() {
+	current := l.head
+	for current != nil {
+		fmt.Printf("%v\n", current.data)
+		current = current.next
+	}
 }
--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 
 func main() {
 	mylist := linkedlist.LinkedList{}
-
+	mylist.Append(2)
+	mylist.Append(1)
+	mylist.Append(3)
+	mylist.Prepend(9)
+	mylist.Prepend(4)
+	mylist.PrintList()
 	len := mylist.GetLength()
-
 	fmt.Println(len)
 }
